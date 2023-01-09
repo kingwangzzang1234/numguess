24034dd014b56bc5c94472d650265c8efcbe8a9c
kingwangzzang1234 <wychoi0408@naver.com>
2023-01-09 07:11:46 +0000

    docs: Add Commentary on numguess.py
    To split User's name and guess from user interaction

--- numguess.py
+++ numguess.py
@@ -10,5 +10,6 @@ print(answer)
 # User interaction
 username = input("Hi there, What is your name?")
 print(f"Hi,{username}! Please be my guest!!")
+# Get and print User's guess
 guess = int(input(f"So {username}, Guess the number(1-100): "))
 print(f'Well choice {username}~~ You picked {guess}!!')
